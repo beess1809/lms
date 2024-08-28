2a632f741646b45278444775f490ac7c5b254698
aditpraset <aditpraset@gmail.com>
2024-08-28 11:32:23 +0700

    update: php ini

--- app/Http/Controllers/Module/TrainingSubController.php
+++ app/Http/Controllers/Module/TrainingSubController.php
@@ -107,6 +107,8 @@ class TrainingSubController extends Controller
         } else {
             DB::beginTransaction();
             try {
+                ini_set('post_max_size', '1024M');
+                ini_set('upload_max_filesize', '1024M');
                 $model = new TrainingSub();
                 $model->title = $request->title;
                 $model->description = $request->description;
